a2f81c9825a275bc6fcfab484720b62287c9a1e2
Alex-PC-home <bur4ik85@mail.ru>
2023-12-31 11:24:07 +0300

    add pytest.ini, edit base_page, locators, main_page, test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -15,6 +15,7 @@ links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pr
          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
+@pytest.mark.myskip
 @pytest.mark.parametrize('link', [
     link if link != 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7'
     else pytest.param(link, marks=pytest.mark.xfail) for link in links])
@@ -23,3 +24,18 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.should_be_product_page()
     page.add_to_basket()
+    page.solve_quiz_and_get_code()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
